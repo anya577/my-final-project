6eb89ae7e823ae448e1ecde54faf9d5e8c76713e
Anna Tukvadze <annatukvadze@yahoo.com>
2025-02-25 22:55:25 +0400

    Data bindings and event bindings

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
+  firstName: string = "John";
+  version: number = 18;
+  isActive: boolean = false;
+  currentDate: Date = new Date();
+  inputType: string = "button";
+  selectedCities: string = '';
 
+  showWelcomeAlert() { 
+    alert ("welcome")
+  }
+
+  showMessage(message: string) { 
+    alert(message)
+  }
 }
+
